0a0b0fc6f2cd311eaba8a6aeeec86afe0bf13f10
henrycjchen <henrycjchen@tencent.com>
2023-04-03 23:02:53 +0800

    feat: add input

--- packages/frontend-react/src/views/channel/channel-content/components/Input.tsx
+++ packages/frontend-react/src/views/channel/channel-content/components/Input.tsx
@@ -1,7 +1,7 @@
 export default function Input() {
   return (
-    <>
-      input
-    </>
+    <div className="h-100px pt-10px pr-20px pl-20px pb-10px">
+      <textarea className="font-15 bg-divider h-100% w-100%" autoFocus />
+    </div>
   );
 }
--- packages/frontend-react/src/views/channel/channel-content/components/Messages.tsx
+++ packages/frontend-react/src/views/channel/channel-content/components/Messages.tsx
@@ -2,9 +2,12 @@ import MessageItem, {Direction} from './MessageItem';
 
 export default function Messages() {
   return (
-    <div className="p-20px">
-      <MessageItem direction={Direction.Right} isPrimary={true} />
-      <MessageItem direction={Direction.Left} />
+    <div className="flex flex-col flex-1">
+      <div className="p-20px flex-1">
+        <MessageItem direction={Direction.Right} isPrimary={true} />
+        <MessageItem direction={Direction.Left} />
+      </div>
+      <div className="h-1px bg-#fff opacity-10"></div>
     </div>
   );
 }
