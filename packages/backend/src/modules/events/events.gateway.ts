9c0f458200a4a3c5f9b5ab8d016d6400605d365a
henrycjchen <henrycjchen@tencent.com>
2023-04-06 00:15:25 +0800

    feat: create socket.io

--- packages/backend/src/modules/events/events.gateway.ts
+++ packages/backend/src/modules/events/events.gateway.ts
@@ -6,9 +6,13 @@ import {
   WebSocketServer,
   WsResponse,
 } from '@nestjs/websockets';
+import { ObjectId } from 'mongoose';
 import { from, Observable } from 'rxjs';
 import { map } from 'rxjs/operators';
 import { Server, Socket } from 'socket.io';
+import { UserService } from '../user/user.service';
+
+const clientCache: { client: Socket; userId?: ObjectId }[] = [];
 
 @WebSocketGateway({
   cors: {
@@ -16,27 +20,39 @@ import { Server, Socket } from 'socket.io';
   },
 })
 export class EventsGateway {
+  constructor(private readonly userService: UserService) {}
   @WebSocketServer()
   server: Server;
 
-  @SubscribeMessage('connect')
-  async connect(
-    @MessageBody() data: any,
-    // @ConnectedSocket() client: Socket
-  ): Promise<string> {
-    // console.log('client', client);
-    return '222';
-  }
-
-  @SubscribeMessage('events')
-  findAll(@MessageBody() data: any): Observable<WsResponse<number>> {
-    return from([1, 2, 3]).pipe(
-      map((item) => ({ event: 'events', data: item }))
+  @SubscribeMessage('message')
+  findAll(@MessageBody() data: any, @ConnectedSocket() client: Socket): void {
+    const toClient = clientCache.find(
+      (cache) => cache.userId.toString() === data.toUserId
     );
+
+    toClient?.client.emit('message', data.message);
   }
 
   @SubscribeMessage('identity')
-  async identity(@MessageBody() data: number): Promise<number> {
-    return data;
+  async identity(
+    @MessageBody() userId: string,
+    @ConnectedSocket() client: Socket
+  ): Promise<string> {
+    const cache = clientCache.find((v) => v.client.id === client.id);
+    try {
+      const user = await this.userService.findOne(userId);
+      cache.userId = user._id;
+    } catch (e) {
+      cache.client.disconnect();
+    }
+    return 'success ' + client.id;
   }
+
+  handleConnection(@ConnectedSocket() client: Socket) {
+    clientCache.push({
+      client,
+    });
+  }
+
+  handleDisconnect(@ConnectedSocket() client: Socket) {}
 }
--- packages/backend/src/modules/events/events.module.ts
+++ packages/backend/src/modules/events/events.module.ts
@@ -1,7 +1,9 @@
 import { Module } from '@nestjs/common';
+import { UserModule } from '../user/user.module';
 import { EventsGateway } from './events.gateway';
 
 @Module({
+  imports: [UserModule],
   providers: [EventsGateway],
 })
 export class EventsModule {}
